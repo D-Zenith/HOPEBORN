ae6549e69888a57656c34b8f76eabd0a586e284c
neehox <n.pinard303@gmail.com>
2020-07-20 14:16:02 -0400

    fixed shadow bug

--- scenes/game/tests/KinematicBody.gd
+++ scenes/game/tests/KinematicBody.gd
@@ -3,5 +3,5 @@ var speed = 10
 
 func _process(delta):
 	var x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var z = - Input.get_action_strength("ui_up") + Input.get_action_strength("ui_down") 
+	var z = - Input.get_action_strength("ui_up") + Input.get_action_strength("ui_down")
 	move_and_slide(Vector3(x,0,z) * speed)
