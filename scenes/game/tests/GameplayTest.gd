db7ec2ebe7fcad0c06694fcbc35d493e84f2a043
neehox <n.pinard303@gmail.com>
2020-07-20 22:58:35 -0400

    i added some stuff ill continue tomorrow

--- scenes/game/tests/GameplayTest.gd
+++ scenes/game/tests/GameplayTest.gd
@@ -1,6 +1,4 @@
-extends Control
-
-
+extends Spatial
 
 func _on_Area_body_entered(body):
 	if body.is_in_group("PLAYER"):
--- scripts/game/menus/fileselect.gd
+++ scripts/game/menus/fileselect.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+export var scene : PackedScene
 
 func _on_Button_pressed():
-	Loader.goto_scene("res://scenes/game/tests/HospitalTest.tscn")
+	Loader.goto_scene(scene.resource_path)
