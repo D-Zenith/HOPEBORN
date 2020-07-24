49563ff7c816a4d5a823a0a69e8cd8d91ecfe5fc
D_Zenith <dlw2002@outlook.com>
2020-07-24 13:42:19 +0200

    Merged Neehox' Loading 'Fade'

--- nodes/player/Player.gd
+++ nodes/player/Player.gd
@@ -2,6 +2,6 @@ extends KinematicBody
 export var speed = 2
 
 func _process(delta):
-	var x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var z = - Input.get_action_strength("ui_up") + Input.get_action_strength("ui_down")
+	var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var z = - Input.get_action_strength("move_up") + Input.get_action_strength("move_down")
 	move_and_slide(Vector3(x,0,z) * speed)
