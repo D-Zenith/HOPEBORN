60d73aec1269be3cf9d23d9513cf3784c1c5bae9
MeltedBrainz <68511851+MeltedBrainz@users.noreply.github.com>
2020-07-27 13:36:57 +0100

    organized some folders

--- scenes/game/tests/GameplayTest.gd
+++
@@ -1,9 +0,0 @@
-extends Spatial
-
-func _on_Area_body_entered(body):
-	if body.is_in_group("PLAYER"):
-		$GameWorld/Room/Camera2.current = true
-
-func _on_Area2_body_entered(body):
-	if body.is_in_group("PLAYER"):
-		$GameWorld/Room/Camera.current = true
