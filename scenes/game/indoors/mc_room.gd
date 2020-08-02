c5ba3c7965b3df76414add3d94e8f87ee2f89de8
MeltedBrainz <68511851+MeltedBrainz@users.noreply.github.com>
2020-08-02 13:39:55 +0100

    added basic pc interaction and animation

--- scenes/game/indoors/mc_room.gd
+++ scenes/game/indoors/mc_room.gd
@@ -1,21 +1,41 @@
 extends Spatial
+export var HintBarNode: NodePath
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+var HintBar
+var proximity_item = ""
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	HintBar = get_node(HintBarNode)
 
 func _on_DoorArea_body_entered(body):
 	if body.is_in_group("PLAYER"):
-		$WorldEnvironment/Door_Group/DoorFrame2/Door2/AnimationPlayer.play("open_door")
+		HintBar.show_hint("Door: Press E to open.")
+		proximity_item = "door"
+
+func _on_DoorArea_body_exited(body):
+	if body.is_in_group("PLAYER"):
+		HintBar.reset()
+		if proximity_item == "door":
+			proximity_item = ""
+
+func _input(event):
+	if event.is_action_pressed("interact"):
+		match proximity_item:
+			"door":
+				$WorldEnvironment/Door_Group/DoorFrame2/Door2/AnimationPlayer.play("open_door")
+			"computer":
+				$WorldEnvironment/Camera/CamAnimation.play("focus_computer")
+				HintBar.show_hint("Computer: Press E to exit")
+				proximity_item = "computer_screen"
+			"computer_screen":
+				$WorldEnvironment/Camera/CamAnimation.play_backwards("focus_computer")
+
+func _on_DesktopArea_body_entered(body):
+	if body.is_in_group("PLAYER"):
+		HintBar.show_hint("Computer: Press E to use.")
+		proximity_item = "computer"
+
+func _on_DesktopArea_body_exited(body):
+	if body.is_in_group("PLAYER"):
+		HintBar.reset()
+		if proximity_item == "computer":
+			proximity_item = ""
