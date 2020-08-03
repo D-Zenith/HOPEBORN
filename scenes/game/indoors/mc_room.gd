35c39d916e16b38dcb34fe50aa285420fb7d90d0
rahulghosh <ghoshr698@gmail.com>
2020-08-04 00:35:44 +0530

    light switch, door exit
    added light switch
    added door exit to area 1
    fixed god ray frame rate issue

--- scenes/game/indoors/mc_room.gd
+++ scenes/game/indoors/mc_room.gd
@@ -1,8 +1,19 @@
 extends Spatial
 export var HintBarNode: NodePath
 
+
 var HintBar
+
+
+# state variable
+var door_open=false
+var lights_on=false
+# state variable end
+
+# utility variable
 var proximity_item = ""
+#utility variable end
+
 func _ready():
 	HintBar = get_node(HintBarNode)
 
@@ -17,18 +28,30 @@ func _on_DoorArea_body_exited(body):
 		if proximity_item == "door":
 			proximity_item = ""
 
+
 func _input(event):
 	if event.is_action_pressed("interact"):
 		match proximity_item:
 			"door":
-				$WorldEnvironment/Door_Group/DoorFrame2/Door2/AnimationPlayer.play("open_door")
+				if door_open:
+					$room_anims.play_backwards("open_door")
+					door_open=false
+				else:
+					door_open=true
+					$room_anims.play("open_door")
 			"computer":
 				$WorldEnvironment/Camera/CamAnimation.play("focus_computer")
 				HintBar.show_hint("Computer: Press E to exit")
 				proximity_item = "computer_screen"
 			"computer_screen":
 				$WorldEnvironment/Camera/CamAnimation.play_backwards("focus_computer")
-
+			"lights":
+				if lights_on:
+					$room_anims.play("lights_off")
+					lights_on=false
+				else:
+					$room_anims.play("lights_on")
+					lights_on=true
 func _on_DesktopArea_body_entered(body):
 	if body.is_in_group("PLAYER"):
 		HintBar.show_hint("Computer: Press E to use.")
@@ -39,3 +62,17 @@ func _on_DesktopArea_body_exited(body):
 		HintBar.reset()
 		if proximity_item == "computer":
 			proximity_item = ""
+
+
+
+
+
+func _on_switch_board_body_entered(body):
+	if body.is_in_group("PLAYER"):
+		HintBar.show_hint("Lights: Press E to use.")
+		proximity_item = "lights"
+
+
+func _on_exit_body_entered(body):
+	if body.name=="Player":
+		Loader.goto_scene("res://scenes/game/map/area1.tscn")
