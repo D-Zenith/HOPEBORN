7a4641f41ceb5bc446f40a013e96889b8d93388b
rahulghosh <ghoshr698@gmail.com>
2020-08-09 23:35:39 +0530

    jump and gravity

--- nodes/player/Player.gd
+++ nodes/player/Player.gd
@@ -1,17 +1,50 @@
 extends KinematicBody
-export var speed = 3.7
+export var speed = 40
+export var jump_str=40
 export var camera_on = true
+export var gravity = -10
+#higher gravity make it slower to walk and shorter jumps
+#tbh the jumps are too fast 
 var dir = []
-
+var y=0
+var inair
 func _ready():
 	$Camera.current = camera_on
+	inair=false
+
+func _input(event):
+	if event.is_action_pressed("jump") and !inair:
+		inair=true
+		y +=jump_str
+# one press actions here
+
 func _process(delta):
-	
-	var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var z = - Input.get_action_strength("move_up") + Input.get_action_strength("move_down")
-	var velocity = Vector3(x,0,z).normalized()
-	move_and_slide(velocity * speed)
+	var x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))*speed
+	y += gravity()
+	var z =  (-Input.get_action_strength("move_up") + Input.get_action_strength("move_down"))*speed
+	#components assigned
+	var velocity = Vector3(x,0,z)
+	velocity.y=y
+	#vel assigned
+	var c = move_and_collide(velocity*delta)
+	#movement 
 	get_input_for_anim()
+	#sync anims
+	if c:
+		var ObjectCollidedWith = c.collider as StaticBody
+		y=0
+		inair=false
+	# collision check can be used for bouncing later
+	
+# made a gravity func in case we need variable gravity later
+func gravity():
+	return gravity
+
+
+
+
+
+
 #	if Input.is_action_just_pressed("cam_orb_left"):
 #		print("orb_left")
 #		$camera_rig.rotation.y=lerp_angle(rotation.y,rotation.y-deg2rad(90),1)
@@ -85,3 +118,5 @@ func get_input_for_anim():
 		Input.is_action_pressed("move_up") and  Input.is_action_pressed("move_down"):
 		$AnimationPlayer.stop()
 		$Sprite3D.frame = 0
+	if Input.is_action_just_pressed("jump"):
+		pass
