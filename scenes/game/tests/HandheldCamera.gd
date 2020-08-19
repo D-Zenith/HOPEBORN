7385e6e7a517fa7c61de0d176d591fa27d8aefc8
D_Zenith <dlw2002@outlook.com>
2020-08-19 14:23:34 +0200

    Almighty push

--- scenes/game/tests/HandheldCamera.gd
+++ scenes/game/tests/HandheldCamera.gd
@@ -5,6 +5,7 @@ export var  speed = 0.05
 export var switch_dir_time = 0.6
 export var rotate = false
 export var rotate_strength = 1.0
+
 var target_v_offset = 0
 var dir = 1
 var rot_dir = -1
@@ -16,10 +17,13 @@ func _process(delta):
 	target_v_offset = rand_range(0, speed) * dir
 	target_rot_x = rand_range(0,2) * rot_dir * rotate_strength
 	target_rot_z = rand_range(0,2) * rot_dir * rotate_strength
+	
 	v_offset = lerp(v_offset, target_v_offset,delta)
+	
 	if rotate:
 		rotation_degrees.x = lerp(rotation_degrees.x, target_rot_x, delta)
 	#	rotation_degrees.z = lerp(rotation_degrees.z, target_rot_z, delta)
+	
 func _ready():
 	ShakeTimer.wait_time = switch_dir_time
 	ShakeTimer.start()
